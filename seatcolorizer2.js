5b016ed41e3b4d01e55424e3cd51fdd6f1b81151
NSemakov <NSemakov@users.noreply.github.com>
2017-10-05 12:09:40 +0300

    Add files via upload

--- seatcolorizer2.js
+++ seatcolorizer2.js
@@ -4,13 +4,21 @@
  * @param elm The {embed,object,iframe} element that points to a compatible svg (map) graphic
  * @param callback The js object that allows reporting seats
  */
+var mapsvgdoc = null;
+//var seatNumber = null;
+//var updateSeatColor = colorSeat(seatNumber);
+
+window.addEventListener('load', function() {
+    new FastClick(document.body);
+}, false);
+
 function SeatColorizer(elm) {
 
     init(elm);
 
     function init(elm) {
 
-        var mapsvgdoc = null;
+        //var mapsvgdoc = null;
         try {
           mapsvgdoc = elm.contentDocument;
         }
@@ -21,13 +29,11 @@ function SeatColorizer(elm) {
         mapurl = elm.src || elm.data;
 
 
-        mapsvgdoc.documentElement.setAttributeNS(null, "focusHighlight", "none"); // prevents showing a focusrect in Opera 9.5
+        mapsvgdoc.documentElement.setAttributeNS(null, 'focusHighlight', 'none'); // prevents showing a focusrect in Opera 9.5
 
-        mapsvgdoc.documentElement.addEventListener("click", seatClick, false);
-   
-/*
-        mapsvgdoc.documentElement.addEventListener('click', function(){ window.webkit.messageHandlers.iosListener.postMessage('click clack!'); });
-       */ 
+        FastClick.attach(mapsvgdoc.documentElement);
+
+        mapsvgdoc.documentElement.addEventListener('click', seatClick, false);
     }
 
     function seatClick(evt) {
@@ -36,23 +42,55 @@ function SeatColorizer(elm) {
         var parent = element.parentNode
         var grandParent = parent.parentNode
 
+        if (element.nodeName == 'text') {
+            parent = element.parentNode.parentNode;
+            grandParent = parent.parentNode
+        }
 
-        if (grandParent && grandParent.getAttribute("id") == "seats") {
+        if (grandParent && grandParent.getAttribute('id') == 'seats') {
 
-            var parentClass = parent.getAttribute("class")
+            var parentClass = parent.getAttribute('class')
+
+            var messageToPost = {
+                 'Id': parent.getAttribute('id')
+            };
 
             if (parentClass) {
-                parent.removeAttribute("class")
-                //window.webkit.messageHandlers.iosListener.postMessage('click clack!');
-                /*callback.selectionChanged(parent.getAttribute("id"), false)*/ 
+                parent.removeAttribute('class')
+                window.webkit.messageHandlers.iosListener.postMessage(messageToPost);
             }
             else {
-                parent.setAttribute("class", "order")
-                //window.webkit.messageHandlers.iosListener.postMessage('click clack!');
-                /*callback.selectionChanged(parent.getAttribute("id"), true)*/ 
+                parent.setAttribute('class', 'order')
+                messageToPost['Class'] = 'order'
+                window.webkit.messageHandlers.iosListener.postMessage(messageToPost);
+            }
+        }
+    }
+}
+
+function colorSeat(seatIds) 
+{
+    for (var i = 0; i < seatIds.length; i++) 
+    {
+        var seatId = seatIds[i]
+        var element = mapsvgdoc.getElementById(seatId);
+        var parent = element.parentNode
+
+        if (parent.getAttribute('id') == 'seats') {
+
+            var parentClass = element.getAttribute('class')
+
+            var messageToPost = {
+                    'id': element.getAttribute('id')
+            };
+
+            if (parentClass) {
+                element.removeAttribute('class')
+            }
+            else {
+                element.setAttribute('class', 'order')
             }
         }
 
     }
-
 }
